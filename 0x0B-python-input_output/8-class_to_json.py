054baa453ed57e1176d8ee7a11166543b8d730bd
Monisola <khairaah@gmail.com>
2022-10-28 08:44:12 -0700

    Input Output in Python

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
-"""This module defines a Python class-to-JSON function"""
+""" Module that returns the dictionary description with a simple
+data structure for a JSON serialization of an object
+"""
 
 
 def class_to_json(obj):
-    """Returns the dictionary representation of a simple data structure"""
-    return obj.
+    """ Function that retuns the dictionary description of an obj """
+
+    res = {}
+    if hasattr(obj, "__dict__"):
+        res = obj.__dict__.copy()
+    return res
